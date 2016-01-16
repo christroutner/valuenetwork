160510df0e0d97fa35d70713c20fadc1865e4625
christroutner <chris.troutner@gmail.com>
2016-01-16 12:07:37 -0800

    minor changes.

--- valuenetwork/templates/api/prototype/js/app.js
+++ valuenetwork/templates/api/prototype/js/app.js
@@ -2,6 +2,48 @@
 serverIp = "192.168.56.101";
 serverPort = "8000";
 
+// ***** BEGIN Backbone Models *****
+var PersonModel = Backbone.Model.extend({
+  /*  
+  defaults: {
+      name: "",
+      nick: "",
+      api_url: "",
+      email: "",
+      address: "unspecified",
+      projects: []
+    }
+    */
+});
+// ***** END Backbone Models ****
+
+// ***** BEGIN Backbone Model-Views *****
+var PersonView = Backbone.View.extend({ //Campaign Model-View
+  tagName: 'div',
+  template: _.template($('#person-template').html()),
+  render: function() {
+    debugger;
+    //Send the Model JSON data to the template and render the template onto the page.
+    this.$el.html(this.template(this.model.toJSON())); 
+
+    return this; //enable chained calls
+  },
+  events: {
+    
+  },
+
+});
+// ***** END Backbone Model-Views *****
+
+// ***** BEGIN Backbone Collections *****
+var PeopleCollection = Backbone.Collection.extend({ model: PersonModel });
+// ***** END Backbone Models *****
+
+
+// ***** BEGIN Backbone Collection-Views *****
+
+// ***** END Backbone Collection-Views *****
+
 $(document).ready( function() {
 
   var debugtrue = false;
@@ -9,7 +51,8 @@ $(document).ready( function() {
   debugger;
   
   if(debugtrue) {
-    $.getJSON('http://'+serverIp+':'+serverPort+'/api/people/?format=json', '', function (data) {
+    //$.getJSON('http://'+serverIp+':'+serverPort+'/api/people/?format=json', '', function (data) {
+    $.getJSON('http://'+serverIp+':'+serverPort+'/api/people', '', function (data) {
       debugger;
     });
   }
